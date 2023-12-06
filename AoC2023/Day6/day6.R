d391dffa205541ab103f2e4fda8b1cfef7ca2f5e
Andrea Barghetti <andreabarghetti@hotmail.com>
2023-12-06 12:08:05 +0100

    day6

--- AoC2023/Day6/day6.R
+++ AoC2023/Day6/day6.R
@@ -1,6 +1,24 @@
 library(tidyverse)
 input <- read_lines("AoC2023/Day6/input.txt")
 
+races = list(time = str_extract_all(input[[1]], "\\d+",simplify = T) %>% as.integer(),
+             distance = str_extract_all(input[[2]], "\\d+", simplify = T) %>% as.integer())
+
 # Part 1 ####
+get_dist = function(time, charge) {
+  charge*time-charge^2
+}
+get_dists <- function(time) {
+  map_dbl(1:time,  ~{
+    get_dist(time, .x)
+  })
+}
+
+pmap_dbl(races, function(time, distance) {
+  sum(get_dists(time) > distance)
+}) %>% prod()
 
 # Part 2 ####
+race <- map(races, str_c,collapse = "") %>% map(as.numeric)
+
+polyroot(c(race$distance+1, -race$time, 1)) %>%  as.numeric() %>% diff() %>% floor()
