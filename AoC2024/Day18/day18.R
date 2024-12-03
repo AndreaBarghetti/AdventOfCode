023b12fe554df6673d9254e68e8664264f52e63b
Andrea Barghetti <andreabarghetti@hotmail.com>
2024-12-03 11:00:48 +0100

    init all days

---
+++ AoC2024/Day18/day18.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+input <- read_lines("AoC2024/Day18/input.txt")
+
+# Part 1 ####
+
+# Part 2 ####
---
+++ AoC2024/Day8/day8.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+input <- read_lines("AoC2024/Day8/input.txt")
+
+# Part 1 ####
+
+# Part 2 ####
