f62b7be01434de3801de876b0943a5ccbfc50f8a
Andrea Barghetti <andreabarghetti@hotmail.com>
2024-12-08 23:04:02 +0100

    d7 bit

--- AoC2024/Day7/day7.R
+++ AoC2024/Day7/day7.R
@@ -1,6 +1,28 @@
 library(tidyverse)
 input <- read_lines("AoC2024/Day7/input.txt")
 
+parse = function(input) {
+  parse_line = function(line) {
+    l = str_split(line,":", simplify = T)
+    y = as.numeric(l[1])
+    x = as.numeric(str_extract_all(l[2], "\\d+", simplify = T))
+    
+    return(list(y=y,x=x))
+  }
+  map(input, parse_line)
+}
+
+input = parse(input)
+
 # Part 1 ####
+operators = list(`+`, `*`)
+
+check_equation = function(y,x) {
+  if ( any(x>y) ) { return(FALSE) }
+  if ( sum(x)>y ) { return(FALSE) }
+  
+
+}
 
 # Part 2 ####
+options(warn =2)
