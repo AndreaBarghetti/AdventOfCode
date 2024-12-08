0c8ea9691458cd7b3f01c00b703ae1a0bce8060c
Andrea Barghetti <andreabarghetti@hotmail.com>
2024-12-09 00:00:40 +0100

    test

--- AoC2024/Day7/day7.R
+++ AoC2024/Day7/day7.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-input <- read_lines("AoC2024/Day7/input.txt")
+input <- read_lines("AoC2024/Day7/test.txt")
 
 parse = function(input) {
   parse_line = function(line) {
@@ -17,12 +17,30 @@ input = parse(input)
 # Part 1 ####
 operators = list(`+`, `*`)
 
+try_operators = function(R, x, y, operators) {
+  for (o in operators) {
+    if (length(x)>1) {
+      R = do.call(o,list(R,x[1]))
+      return(try_operators(R,x[-1], y, operators))
+    } else {
+      R = do.call(o,list(R,x[1]))
+      if (R == y) {return(TRUE)} else {return(FALSE)} 
+    }
+  }
+}
+
+map_lgl(input, ~{
+  try_operators(.x$x[1],.x$x[-1], .x$y, operators)
+})
+
+
 check_equation = function(y,x) {
   if ( any(x>y) ) { return(FALSE) }
   if ( sum(x)>y ) { return(FALSE) }
-  
-
+  return(TRUE)
 }
 
+
+
+
 # Part 2 ####
-options(warn =2)
