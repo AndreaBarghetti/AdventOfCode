a5e3b824a07b6d5dedd8d14cb14943f6620bd74a
Andrea Barghetti <andreabarghetti@hotmail.com>
2024-12-16 23:34:26 +0100

    d16 init

--- AoC2024/Day16/day16.R
+++ AoC2024/Day16/day16.R
@@ -1,6 +1,88 @@
 library(tidyverse)
 input <- read_lines("AoC2024/Day16/input.txt")
 
+map = str_split(input,"", simplify = T)
+
+# source("utils/matrix_helpers.R")
+#source("utils/vis_helpers.R")
+# print_matrix(map)
+# plot_matrix(map) +
+#   scale_fill_manual(values = c("white","black","red","red"))
+
 # Part 1 ####
+
+# works on example
+# must make it efficient to run on real input
+run_maze = function(map) { 
+  
+  best_score = Inf
+  
+  moves = list(
+    U = c(-1,0),
+    R = c(0,1),
+    D = c(1,0),
+    L = c(0,-1)
+  )
+  
+  turn = function(dir, side) {
+    
+    dirs = c("U","R","D","L")
+    
+    i = which(dirs == dir)
+    if (side=="L"){i = i-1; i = (i-1)%%4+1}
+    if (side=="R"){i = i+1; i = (i-1)%%4+1}
+    dirs[i]
+  }
+  
+  start = which(map=="S", arr.ind = T)
+  dir = "L"
+  
+  status = list(pos=start, 
+                dir=dir, 
+                score=0)
+  
+  get_nexts = function(status) {
+    
+    list(
+      run = list(pos = status$pos + moves[[status$dir]], 
+                 dir = status$dir, 
+                 score = status$score+1),
+      
+      turnL = list(pos = status$pos, 
+                   dir = turn(status$dir, "L"), 
+                   score=status$score+1000),
+      
+      turnR = list(pos = status$pos, 
+                   dir = turn(status$dir, "R"), 
+                   score = status$score+1000)
+      
+    )
+  }
+  
+  queue = collections::priority_queue()
+  queue$push(status, -status$score)
+  
+  
+  while (queue$size()>0){
+    status = queue$pop()
+    
+    if (map[status$pos]=="#") {next}
+    if (status$score>best_score) {next}
+    if (map[status$pos]=="E") {best_score = status$score; next}
+    
+    next_s = get_nexts(status)
+    for (s in next_s) {
+      queue$push(s, -s$score)
+    }
+    
+    
+  }
+  
+  return(best_score)
+  
+}
+
+run_maze(map)
+
 
 # Part 2 ####
