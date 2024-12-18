fc4f27fb3e75d642b392d5e8ed4506507a6f87be
Andrea Barghetti <andreabarghetti@hotmail.com>
2024-12-19 00:12:36 +0100

    d18 init

--- AoC2024/Day18/day18.R
+++ AoC2024/Day18/day18.R
@@ -1,6 +1,14 @@
 library(tidyverse)
 input <- read_lines("AoC2024/Day18/input.txt")
 
+parse = function(input) {
+  str_extract_all(input,"\\d+",simplify = T) %>% 
+    apply(2,as.integer) + 1
+}
+bytes = parse(input)
+map = matrix(F, nrow = 71, ncol = 71)
+map[bytes[1:1024]] <- T
+
 # Part 1 ####
 
 # Part 2 ####
