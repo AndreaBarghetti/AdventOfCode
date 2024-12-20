3e82841381887e0197318bbd833531c3d9bc115a
Andrea Barghetti <andreabarghetti@hotmail.com>
2024-12-20 13:05:24 +0100

    d19 pt1

--- AoC2024/Day19/day19.R
+++ AoC2024/Day19/day19.R
@@ -1,6 +1,29 @@
 library(tidyverse)
 input <- read_lines("AoC2024/Day19/input.txt")
 
+parse = function(input) {
+  patterns = input[1] %>% str_split(", ") %>% unlist()
+  designs = input[-c(1:2)] %>% unlist()
+  return(list(patterns=patterns,designs=designs))
+}
+
+patterns = parse(input)$patterns
+designs = parse(input)$designs
+
 # Part 1 ####
+match_design = function(design, patterns) {
+  if (any(patterns==design)) {return(T)}
+  # p=patterns[1]
+  for (p in patterns) {
+    if (str_starts(design,p)) {
+      cut_design = str_remove(design,p) 
+      if (match_design(cut_design, patterns)) {return(T)}
+    }
+  }
+  return(FALSE)
+}
+
+sum(map_lgl(designs, match_design, patterns))
+#342
 
 # Part 2 ####
