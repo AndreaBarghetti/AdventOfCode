ea8b95d77a5e9d032f3dbd1f557dffdf407b175c
Andrea Barghetti <andreabarghetti@hotmail.com>
2025-12-08 20:07:29 +0100

    d15 2015

--- AoC2015/Day15/day15.R
+++ AoC2015/Day15/day15.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 input <- read_lines("AoC2015/Day15/input.txt")
 
-parse_input <- function(input) {
+parse_properties <- function(input) {
   ingr <- str_extract(input, "^\\w+")
   values = str_extract_all(input, "-*\\d+", simplify = T)
   values = matrix(as.numeric(values), nrow(values))
@@ -10,18 +10,51 @@ parse_input <- function(input) {
   values
 }
 
-properties <- parse_input(input)
+properties <- parse_properties(input)
 
-get_score <- function(Sp,Bu,Ch) {
+# part 1 ####
+get_score <- function(Sp,Bu,Ch, properties) {
   
   coefs = c(Sp,Bu,Ch)
   if(sum(coefs)>100){return(0)}
   coefs = c(coefs, 100-sum(coefs))
   
-  score = (properties *c(coefs)) %>% apply(2,sum) %>% sapply(max,0) %>% prod()
-
+  score = t(properties) %*% coefs %>% head(4) %>% pmax(0) %>% prod()
+  
   score
 }
 
-res <- optim(par = c(10,10,10), fn = function(x) {get_score(x[1], x[2], x[3])}, lower=c(0,0,0), upper = c(100,100,100),
-      method="L-BFGS-B", hessian = TRUE)
+#brute force try all combos
+find_best = function(properties) {
+  best = 0 
+  for (x in 1:100) {
+    for (y in 1:100) {
+      for ( z in 1:100) {
+        score = get_score(x,y,z, properties)
+        if (score > best) {
+          best = score
+          best_vals = c(x,y,z)
+        }
+      }
+    }
+  }
+  return(list(vals = best_vals,
+              score = best))
+}
+find_best(properties)
+
+# part 2 ####
+get_score <- function(Sp,Bu,Ch, properties) {
+  
+  coefs = c(Sp,Bu,Ch)
+  if(sum(coefs)>100){return(0)}
+  coefs = c(coefs, 100-sum(coefs))
+  
+  score = t(properties) %*% coefs  %>% pmax(0) %>% head(4) %>%  prod()
+  calories = t(properties) %*% coefs  %>% pmax(0) %>% pluck(5)
+  if (calories != 500) {return(0)}
+  score
+}
+
+find_best(properties)
+
