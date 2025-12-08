d04f7b86d5499b5443b9f64f304caca213bb74ab
Andrea Barghetti <andreabarghetti@hotmail.com>
2025-12-08 11:28:30 +0100

    d8

--- AoC2025/Day8/day8.R
+++ AoC2025/Day8/day8.R
@@ -1,6 +1,59 @@
 library(tidyverse)
-input <- read_lines("AoC2025/Day8/input.txt")
+library(igraph)
+
+jboxes <- read_lines("AoC2025/Day8/test") %>% 
+  str_extract_all('\\d+', simplify = T) %>% 
+  apply(2,as.numeric)
 
 # Part 1 ####
+make_dist_matrix = function(jboxes) {
+  D = dist(jboxes, diag = T)
+  D=as.matrix(D)
+  diag(D) <- NA
+  D[upper.tri(D)] <- NA
+  D
+}
+
+get_closest_pairs = function(jboxes, topN) {
+  
+  D = make_dist_matrix(jboxes)
+  topNd = sort(D, decreasing = F) %>% head(topN)
+  pairs = map(topNd, ~{
+    which(D==.x,arr.ind = T) %>% as.integer()
+  })
+  pairs = do.call(rbind, pairs)
+  pairs
+}
+
+connect_jboxes = function(jboxes, N) {
+  pairs = get_closest_pairs(jboxes,N)
+  g = graph_from_edgelist(pairs, directed = FALSE)
+  g
+}
+
+g = connect_jboxes(jboxes, 1000)
+components(g)$csize %>% sort(decreasing = T) %>% head(3) %>% 
+  prod()
 
 # Part 2 ####
+connect_all_jboxes = function(jboxes) {
+  
+  g = make_empty_graph(n = nrow(jboxes), directed = FALSE) 
+  
+  D = make_dist_matrix(jboxes)
+  topNd = sort(D, decreasing = F)
+  
+  for (d in topNd) {
+    vertices = which(D==d, arr.ind = T) %>% as.integer()
+    g <- add_edges(g, vertices)
+    if (components(g)$no == 1) {break}
+  }
+  
+  #return last 2 jboxes connected
+  vertices
+}
+
+last2 = connect_all_jboxes(jboxes)
+
+jboxes[last2,1] %>% prod() %>% 
+  format(scientific = F)
