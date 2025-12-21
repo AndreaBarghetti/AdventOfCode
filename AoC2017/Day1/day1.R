5b042d7c5d6b65a11c8359424cf32c4955635bfa
Andrea Barghetti <andreabarghetti@hotmail.com>
2025-12-22 00:00:06 +0100

    d1 2017

--- AoC2017/Day1/day1.R
+++ AoC2017/Day1/day1.R
@@ -1,5 +1,17 @@
 library(tidyverse)
 input <- read_lines('AoC2017/Day1/input.txt')
 
+digits = str_split(input,"", simplify = T) %>% as.integer()
+
 # Part 1 ----
+sum(digits[digits==lead(digits, default = digits[1])])
+
 # Part 2 ----
+count_digits = function(digits) {
+  L=length(digits)
+  digits2 = digits[c((L/2+1):L,1:(L/2))]
+  sum(digits[digits==digits2])
+}
+
+count_digits(digits)
+
