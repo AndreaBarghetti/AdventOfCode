3e7e90d996e510626c8ace15ce184b2d47eb3e26
Andrea Barghetti <andreabarghetti@hotmail.com>
2025-12-21 16:19:22 +0100

    d25 2020

--- AoC2020/Day25/solution.R
+++ AoC2020/Day25/solution.R
@@ -1,5 +1,32 @@
 library(tidyverse)
 
 # Day 25: Combo Breaker ####
+input=read_lines('AoC2020/Day25/input.txt')
+
+card_pk = input[1] %>% as.numeric()
+door_pk = input[2] %>% as.numeric()
+
 # * - part 1 - *####
-# * - part 2 - *####
+gen_pk = function(sn, loop_size) {
+  v=1
+  for (i in 1:loop_size) {
+    v = (v*sn)%%20201227
+  }
+  v
+}
+
+find_loop_size =function(pk) {
+  v=1
+  i=0
+  while(v!=pk) {
+    i=i+1
+    v = (v*7)%%20201227
+  }
+  i
+}
+
+card_loop_size = find_loop_size(card_pk)
+door_loop_size = find_loop_size(door_pk)
+
+gen_pk(door_pk, card_loop_size)
+gen_pk(card_pk, door_loop_size)
