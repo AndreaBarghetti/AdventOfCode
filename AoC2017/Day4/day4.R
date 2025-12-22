54f43d7a01070f905e8b57d0e49581f8dde72c6a
Andrea Barghetti <andreabarghetti@hotmail.com>
2025-12-22 23:57:58 +0100

    d4 2017

--- AoC2017/Day4/day4.R
+++ AoC2017/Day4/day4.R
@@ -1,4 +1,22 @@
 library(tidyverse)
 input <- read_lines('AoC2017/Day4/input.txt')
+
+passphrases = input %>% str_split(" ")
+
 # Part 1 ----
+validate_passphrase = function(passphrase) {
+  !any(passphrase %>% table()>1)
+}
+
+sum(map_lgl(passphrases, validate_passphrase))
+
 # Part 2 ----
+sort_letters = function(passphrase) {
+  map_chr(passphrase, function(word) {
+    str_split(word,"", simplify = T) %>% sort() %>% paste0(collapse = "")
+  })
+}
+
+map(passphrases, sort_letters) %>% 
+  map_lgl(validate_passphrase) %>% 
+  sum()
